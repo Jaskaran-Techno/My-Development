dc8f80126f102085a055741341f23166f777ad0e
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-10-18 20:37:51 +0530

    final commit

---
+++ alertpromptconfirm.js
@@ -0,0 +1,22 @@
+// alert
+alert("This page is not responding");
+
+// prompt
+let a = prompt("Enter a value");
+
+// confirm
+let write = confirm("Do you want to write");
+if (write) {
+    document.write(a)
+}
+else {
+    document.write("Yes i want to write")
+}
+
+
+
+
+
+
+
+

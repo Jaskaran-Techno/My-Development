ed7aaa4ade96b9b8beaac6792eab8cbc61ea1ad8
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-10-21 18:46:51 +0530

    final commit

--- events.js
+++ events.js
@@ -2,3 +2,46 @@ let a = document.getElementsByName("container");
 a.onclick = () => {
   console.log("");
 };
+
+// addeventlistener
+
+btn.addEventListener("click", function (e) {
+  alert("hello1");
+});
+
+btn.addEventListener("click", function (e) {
+  alert("hello2");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

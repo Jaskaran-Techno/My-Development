b010a0398805a1e8e75d2e9d91cb19765de4eca9
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-10-24 17:19:48 +0530

    final commit

---
+++ promises.js
@@ -0,0 +1,16 @@
+// Javascript promises.
+// State and result are the two properties of promises.
+
+let promise = new Promise(function (resolve, reject) {
+    alert("hello 4");
+    resolve(55);
+  });
+  
+  console.log("hello 1");
+  setTimeout(function () {
+    console.log("hello 2");
+  }, 2000);
+  
+  console.log("hello 3");
+  console.log(promise);
+  

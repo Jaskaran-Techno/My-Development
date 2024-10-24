ae4109cc53768b3097f645768e28dcad01371ca0
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-10-24 16:41:30 +0530

    final commit

--- asynchronous.js
+++ asynchronous.js
@@ -3,3 +3,17 @@ setTimeout(function () {
     console.log("hello i am good");
   }, 3000);
   
+// callbacks
+// callback hell
+  
+  const animateAll = (animate) => {
+    setTimeout(() => {
+        animate(words[0]);
+        setTimeout(() => {
+            animate(words[1]);
+            setTimeout(() => {
+                animate(words[2]);
+            }, 1000)
+        }, 1000)
+    }, 1000)
+  }

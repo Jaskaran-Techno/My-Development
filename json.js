c7aeeb08c30a42aa3f3ddf1b8ce71fe3f101d217
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-10-27 16:40:12 +0530

    JSON

---
+++ json.js
@@ -0,0 +1,11 @@
+let jsonObj = {
+    name: "Jaskaran",
+    biceps: "16 inches",
+    height: "5.10",
+    weight: "69 kg",
+  };
+  console.log(jsonObj);
+  
+  let myJson = JSON.stringify(jsonObj);
+  console.log(myJson);
+  

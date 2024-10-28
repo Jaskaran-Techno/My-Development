2cde26df25b0cb34c6781cddf4b947452b1bce0d
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-10-28 19:14:21 +0530

    try & catch

---
+++ trycatch.js
@@ -0,0 +1,22 @@
+setTimeout(() => {
+    console.log("Hacking wifi");
+  }, 1000);
+  
+  try {
+    console.log("Peter");
+  } catch (error) {
+    console.log(error);
+  }
+  
+  setTimeout(() => {
+    console.log("Fetching username and password... Pleasde wait");
+  }, 2000);
+  
+  setTimeout(() => {
+    console.log("Hacking Facebook & Instagram Id... Please wait");
+  }, 3000);
+  
+  setTimeout(() => {
+    console.log("hacking complete... Getting access to remote files...");
+  }, 4000);
+  

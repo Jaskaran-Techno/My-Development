abb8b3797a5f71c9b888bddde0ae223dc2ee900e
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-11-02 18:42:49 +0530

    final commit

--- promisehandlers.js
+++ promisehandlers.js
@@ -13,3 +13,18 @@ let p1 = new promise(function (resolve, reject) {
     console.log("Hurray, this works fine");
   });
   
+  let p2 = new promise(function (resolve, reject) {
+    alert("give me the value");
+    setTimeout(() => {
+      resolve(1);
+    }, 2000);
+  });
+  
+  p2.then(() => {
+    console.log("Hey this promise is resolved");
+  });
+  
+  p2.then(() => {
+    console.log("Hurray, this works fine");
+  });
+  
